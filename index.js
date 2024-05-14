ad0eef977259cb5fdb8aa76df6928e63b0d5dd5d
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-14 14:23:31 -0400

    Add Fetch Request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
